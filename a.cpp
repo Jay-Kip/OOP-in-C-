44a80da1a8e8773f4e462f68147b154cb25df8d5
Jay-Kip <hanningtonkiptoo14@gmail.com>
2024-03-23 16:09:40 +0300

    Creates a class Employee and assigns name to first object employee

--- a.cpp
+++ a.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+using std::string;
 
-class {
+
+class Employee {
 	public:
+		string Name;
+		string Company;
+		int Age;
+
+		void intro() {
+			std::cout<<"Name - " << Name << std::endl;
+			std::cout<<" Company - " << Company << std::endl;
+			std::cout<<"Age - "  << Age << std::endl;
+		};
 
 
-}
+
+};
 
 
 int main()
 {
+	Employee emp_1;
+	emp_1.Name = "Jay";
+	emp_1.Company = "Google";
+	emp_1.Age = 22;
+	
+
+	std::cout << "Employee 1: " << emp_1.Name << std::endl;
+
+
+
 	return (0);
 }
