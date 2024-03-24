62e74602e1c1dd8fcbbf7f6af6aaf229c787173a
Jay-Kip <hanningtonkiptoo14@gmail.com>
2024-03-24 12:33:45 +0300

    Using setter and getter to access encapsulated attributes

--- e.cpp
+++ e.cpp
@@ -3,10 +3,35 @@ using std::string;
 
 
 class Employee {
-	public:
+	private:
 		string Name;
 		string Company;
 		int Age;
+
+	public:
+		void setName (string name) {
+			Name = name;
+		}
+
+		string getName() { //getter
+			return (Name);
+		}
+
+		void setCompany (string company) { //setter
+			Company = company;
+		}
+
+		string getCompany () {
+			return (Company);
+		}
+
+		void setAge (int age) {
+			Age = age;
+		}
+
+		int getAge() {
+			return (Age);
+		}
 
 		void intro() {
 			std::cout<<"Name - " << Name << std::endl;
@@ -28,9 +53,11 @@ class Employee {
 
 int main()
 {
-	Employee emp_1 = Employee("Jay", "Microsoft", 22)u;
+	Employee emp_1 = Employee("Jay", "Microsoft", 22);
 	emp_1.intro();
 
+	emp_1.setAge(56);
+	std::cout<< emp_1.getName() << " is " <<emp_1.getAge()<< " years old" <<std::endl;
 	std::cout << "------------" << std::endl;
 	
 
@@ -38,6 +65,10 @@ int main()
 
 	Employee emp_2 = Employee("Mike", "Samsung" , 24);
 	emp_2.intro();
+
+	emp_2.setName("Naomy");
+	emp_2.setCompany("Azure");
+	std::cout << emp_2.getName() << " works for  " << emp_2.getCompany() << std::endl;
 
 
 	return (0);
